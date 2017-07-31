49a035f212c28aa210d3aa17219a49d04e1b072b
OpenAcousticDevices <theteam@openacousticdevices.info>
2017-07-31 15:09:35 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,198 @@
+/****************************************************************************
+ * app.js
+ * openacousticdevices.info
+ * June 2017
+ *****************************************************************************/
+
+'use strict';
+
+/*global document */
+
+var strftime = require('strftime').utc();
+var audiomoth = require('audiomoth-hid');
+
+/* UI components */
+
+var timeDisplay = document.getElementById('time-display');
+var idDisplay = document.getElementById('id-display');
+var batteryDisplay = document.getElementById('battery-display');
+
+var setTimeButton = document.getElementById('set-time-button');
+
+/* Time display functions */
+
+function initialiseDisplay() {
+
+    timeDisplay.value = "00:00:00 01/01/1970";
+
+}
+
+function disableDisplay() {
+
+    timeDisplay.style.color = "lightgrey";
+
+    idDisplay.style.color = "lightgrey";
+
+    batteryDisplay.style.color = "lightgrey";
+
+    setTimeButton.disabled = true;
+
+}
+
+function enableDisplayAndShowTime(date) {
+
+    var strftimeUTC = strftime.timezone(0);
+
+    timeDisplay.textContent = strftimeUTC("%H:%M:%S %d/%m/%Y", date);
+
+    timeDisplay.style.color = "black";
+
+    setTimeButton.disabled = false;
+
+}
+
+/* Battery display functions */
+
+function enableDisplayAndShowBatteryState(batteryState) {
+
+    batteryDisplay.textContent = batteryState;
+
+    batteryDisplay.style.color = "black";
+
+}
+
+/* ID display functions */
+
+function enableDisplayAndShowID(id) {
+
+    if (idDisplay !== id) {
+
+        idDisplay.value = id;
+
+    }
+
+    idDisplay.style.color = "black";
+
+}
+
+/* Error response */
+
+function errorOccurred(err) {
+
+    console.error(err);
+
+    disableDisplay();
+
+}
+
+
+/* Device interaction functions */
+
+function requestBatteryState() {
+
+    audiomoth.getBatteryState(function (err, batteryState) {
+
+        if (err) {
+
+            errorOccurred(err);
+
+        } else if (batteryState === null) {
+
+            disableDisplay();
+
+        } else {
+
+            enableDisplayAndShowBatteryState(batteryState);
+
+        }
+
+    });
+
+}
+
+function requestID() {
+
+    audiomoth.getID(function (err, id) {
+
+        if (err) {
+
+            errorOccurred(err);
+
+        } else if (id === null) {
+
+            disableDisplay();
+
+        } else {
+
+            enableDisplayAndShowID(id);
+
+            requestBatteryState();
+
+        }
+
+    });
+
+}
+
+function requestTime() {
+
+    audiomoth.getTime(function (err, date) {
+
+        if (err) {
+
+            errorOccurred(err);
+
+        } else if (date === null) {
+
+            disableDisplay();
+
+        } else {
+
+            enableDisplayAndShowTime(date);
+
+            requestID();
+
+        }
+
+        setTimeout(requestTime, 1000);
+
+    });
+
+}
+
+function setTime() {
+
+    audiomoth.setTime(new Date(), function (err, date) {
+
+        if (err) {
+
+            errorOccurred(err);
+
+        } else if (date === null) {
+
+            disableDisplay();
+
+        } else {
+
+            enableDisplayAndShowTime(date);
+
+            setTimeButton.style.color = "green";
+            setTimeout(function () {
+                setTimeButton.style.color = "";
+            }, 1000);
+
+        }
+
+    });
+
+}
+
+/* Main code entry point */
+
+disableDisplay();
+
+initialiseDisplay();
+
+setTimeButton.addEventListener('click', setTime);
+
+setTimeout(requestTime, 1000);
---
+++ main.js
@@ -0,0 +1,93 @@
+/****************************************************************************
+ * main.js
+ * openacousticdevices.info
+ * June 2017
+ *****************************************************************************/
+
+'use strict';
+
+/*jslint nomen: true*/
+
+var electron = require('electron');
+var app = electron.app;
+
+var BrowserWindow = electron.BrowserWindow;
+var Menu = electron.Menu;
+var shell = electron.shell;
+
+require('electron-debug')({
+    showDevTools: "undocked"
+});
+
+var path = require('path');
+
+function openAboutWindow() {
+
+    var aboutWindow = new BrowserWindow({
+        width: 400,
+        height: 300,
+        resizable: false,
+        fullscreenable: false,
+        icon: path.join(__dirname, "/build/icon.ico")
+    });
+
+    aboutWindow.setMenu(null);
+    aboutWindow.loadURL("file://" + __dirname + "/about.html");
+
+}
+
+app.on('ready', function () {
+
+    var mainWindow, menuTemplate, menu, windowHeight = 280;
+
+    if (process.platform === 'darwin') {
+        windowHeight = 242;
+    }
+
+    if (process.platform === 'linux') {
+        windowHeight = 240;
+    }
+
+    mainWindow = new BrowserWindow({
+        width: 565,
+        height: windowHeight,
+        resizable: false,
+        fullscreenable: false,
+        icon: path.join(__dirname, "/build/icon.ico")
+    });
+
+    menuTemplate = [{
+        label: "File",
+        submenu: [{
+            label: "Quit",
+            accelerator: "CommandOrControl+Q",
+            click: function () {
+                app.quit();
+            }
+        }]
+    }, {
+        label: "Help",
+        submenu: [{
+            label: "Open Acoustic Devices website",
+            click: function () {
+                shell.openExternal("https://openacousticdevices.info");
+            }
+        }, {
+            label: "About",
+            click: function () {
+                openAboutWindow();
+            }
+        }]
+    }];
+
+    menu = Menu.buildFromTemplate(menuTemplate);
+
+    Menu.setApplicationMenu(menu);
+
+    mainWindow.loadURL("file://" + __dirname + "/index.html");
+
+});
+
+app.on('window-all-closed', function () {
+    app.quit();
+});
